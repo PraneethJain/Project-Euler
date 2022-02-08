5b8fbbe8ec2cdaab541346317267f3f0a8dd2570
PraneethJain <praneethjain005@gmail.com>
2022-02-08 12:04:54 +0530

    Update Problem_74.py

---
+++ Solutions/Problem_74.py
@@ -0,0 +1,25 @@
+from time import time
+
+f=[1,1,2,6,24,120,720,5040,40320,362880]
+def sum_fac(x):
+    return sum(f[int(i)] for i in str(x))
+
+t1=time()
+ans=0
+dic={}
+for num in range(10**6):
+    L=[num]
+    i=0
+    while sum_fac(L[-1]) not in L:
+        if L[-1] in dic:
+            temp=dic[L[-1]][1:]
+            L.extend(temp)
+            break
+        L.append(sum_fac(L[i]))
+        i+=1
+    dic[num]=L
+    if len(L)==60:
+        ans+=1
+print(ans)
+print(f'Process completed in {time()-t1}s')
+# print(dic)
