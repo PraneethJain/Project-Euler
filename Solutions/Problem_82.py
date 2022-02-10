7eb176e2fc08de5a363894fd5deb6a1783a73206
PraneethJain <praneethjain005@gmail.com>
2022-02-10 12:17:17 +0530

    Created 82

---
+++ Solutions/Problem_82.py
@@ -0,0 +1,11 @@
+from rich import print
+from time import time
+
+
+with open(r'C:\users\prane\Documents\Stuff\Python\Files_Input\p082_matrix.txt') as f:
+    file=f.readlines()
+file=[ele.strip() for ele in file]
+for i,ele in enumerate(file):
+    file[i]=list(map(int,ele.split(',')))
+
+print(file)
