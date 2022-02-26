dbcf13b913c74f54582a3bb9ba40fb4f4db4a810
PraneethJain <praneethjain005@gmail.com>
2022-02-26 10:35:41 +0530

    Formatted

--- Solutions/Problem_01.py
+++ Solutions/Problem_01.py
@@ -1,5 +1,5 @@
 from time import time
 
-t1=time()
-print(sum(i for i in range(1000) if i%3==0 or i%5==0))
-print(f'Process completeed in {time()-t1}s')
+t1 = time()
+print(sum(i for i in range(1000) if i % 3 == 0 or i % 5 == 0))
+print(f"Process completeed in {time()-t1}s")
