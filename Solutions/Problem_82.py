c862f53d6a022006112b07ab0f0e1e511e3ff508
PraneethJain <praneethjain005@gmail.com>
2022-02-26 10:36:32 +0530

    commented out

--- Solutions/Problem_82.py
+++ Solutions/Problem_82.py
@@ -1,11 +1,12 @@
-from rich import print
+# from rich import print
 from time import time
 
+t1=time()
+# with open(r'C:\users\prane\Documents\Stuff\Python\Files_Input\p082_matrix.txt') as f:
+#     file=f.readlines()
+# file=[ele.strip() for ele in file]
+# for i,ele in enumerate(file):
+#     file[i]=list(map(int,ele.split(',')))
 
-with open(r'C:\users\prane\Documents\Stuff\Python\Files_Input\p082_matrix.txt') as f:
-    file=f.readlines()
-file=[ele.strip() for ele in file]
-for i,ele in enumerate(file):
-    file[i]=list(map(int,ele.split(',')))
-
-print(file)
+# print(file)
+print(f'Process completed in {time()-t1}s')
