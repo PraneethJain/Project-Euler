1fdf47ab90f90800bc5563e650ae51621d1e51c9
PraneethJain <praneethjain005@gmail.com>
2022-03-05 10:49:59 +0530

    did 83

---
+++ Solutions/problem_83.py
@@ -0,0 +1,24 @@
+from rich import print
+from time import time
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+
+t1 = time()
+with open(r"files\p083_matrix.txt") as f:
+    matrix = f.readlines()
+matrix = [ele.strip() for ele in matrix]
+for i, ele in enumerate(matrix):
+    matrix[i] = list(map(int, ele.split(",")))
+
+grid = Grid(matrix=matrix)
+start = grid.node(0, 0)
+end = grid.node(79, 79)
+finder = AStarFinder()
+path, _ = finder.find_path(start, end, grid)
+
+sum = 0
+for column, row in path:
+    sum += matrix[column][row]
+
+print(sum)
+print(f"Process completed in time {time()-t1}s")
