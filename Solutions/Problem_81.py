079684d29dec49a9fb8e36480978cfbfe0cba01b
PraneethJain <praneethjain005@gmail.com>
2022-03-05 10:34:45 +0530

    improved 81

--- Solutions/Problem_81.py
+++ Solutions/Problem_81.py
@@ -1,20 +1,20 @@
 from time import time
 from rich import print
 
-t1=time()
-with open(r'C:\users\prane\Documents\Stuff\Python\Files_Input\p081_matrix.txt') as f:
-    file=f.readlines()
-file=[ele.strip() for ele in file]
-for i,ele in enumerate(file):
-    file[i]=list(map(int,ele.split(',')))
+t1 = time()
+with open(r"files\p081_matrix.txt") as f:
+    matrix = f.readlines()
+matrix = [ele.strip() for ele in matrix]
+for i, ele in enumerate(matrix):
+    matrix[i] = list(map(int, ele.split(",")))
 
-for i in range(78,-1,-1): 
-    file[79][i] += file[79][i+1]
-    file[i][79] += file[i+1][79]
+for i in range(78, -1, -1):
+    matrix[79][i] += matrix[79][i + 1]
+    matrix[i][79] += matrix[i + 1][79]
 
-for i in range(78,-1,-1):
-    for j in range(78,-1,-1):
-        file[i][j]+=min(file[i+1][j],file[i][j+1])
+for i in range(78, -1, -1):
+    for j in range(78, -1, -1):
+        matrix[i][j] += min(matrix[i + 1][j], matrix[i][j + 1])
 
-print(file[0][0])
-print(f'Process completed in {time()-t1}s')
+print(matrix[0][0])
+print(f"Process completed in {time()-t1}s")
